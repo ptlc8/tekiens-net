55c25c9de737c494ff5d1ccbcda8f934cf5e6e53
Ambi <ptlc.ptlc.ptlc@gmail.com>
2023-07-01 17:41:46 +0200

    ajout d'un peu de css et de contenu

--- src/assos.js
+++ src/assos.js
@@ -3,12 +3,12 @@ sendRequest("GET", "assos.json")
 	var assos = JSON.parse(res);
 	var assosDiv = document.getElementById("associations");
 	for (let asso of assos) {
-		assosDiv.appendChild(createElement("div", {}, [
+		assosDiv.appendChild(createElement("div", {className: "card asso"}, [
 			createElement("h3", {}, asso.emoji + " " + asso.name[asso.name.length - 1] + " - " + asso.theme),
 			createElement("ul", {}, Object.entries(asso.socials).map(social =>
 				createElement("li", {}, [createElement("a", {href: social[1], target: "_blank"}, social[0])])
 			)),
-			createElement("span", {}, asso.local ? ("Local : " + asso.local) : "")
+			createElement("span", {}, asso.local ? ("Local : " + asso.local + " - " + asso.campus) : asso.campus)
 		]));
 	}
 });
