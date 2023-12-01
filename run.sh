4154eb786cd1412bbb43619e81dbc96b2932e78a
Ambi <ptlc.ptlc.ptlc@gmail.com>
2023-12-01 19:08:31 +0100

    amélioration du run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-cd src/front
-npm run build
-cd ..
-venv/bin/flask run
+case $1 in
+build)
+    cd src/front
+    npm run build
+    ;;
+dev)
+    cd src/front
+    npm run dev
+    ;;
+start)
+    cd src
+    venv/bin/flask run
+    ;;
+"")
+    cd src/front
+    npm run build
+    cd ..
+    venv/bin/flask run
+    ;;
+*)
+    echo "Usage: $0 {build|dev|start|}"
+    exit 1
+    ;;
+esac
