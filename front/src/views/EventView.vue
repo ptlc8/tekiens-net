07a75a390fcac6dc8bae5b32122e04a359dcbe51
Ambi <ptlc.ptlc.ptlc@gmail.com>
2024-09-24 01:00:09 +0200

    Fix affichage durée d'un évent

--- front/src/views/EventView.vue
+++ front/src/views/EventView.vue
@@ -44,7 +44,7 @@ export default {
             var days = Math.floor(this.event.duration / 60 / 24);
             var hours = Math.floor(this.event.duration / 60) % 24;
             var minutes = this.event.duration % 60;
-            return `${days}j ${hours}h ${minutes}min`.replace(/0j /, '').replace(/0h /, '').replace(/ 0min/, '');
+            return `${days}j ${hours}h ${minutes}min`.replace(/ 0min/, '').replace(/ 0h/, '').replace(/^0j /, '');
         }
     },
     methods: {
