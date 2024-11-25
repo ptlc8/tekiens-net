acb66122799f4ba6e53918cf4e2126fd18a16441
Ambi <ptlc.ptlc.ptlc@gmail.com>
2024-11-25 03:30:15 +0100

    Back-end : fix page contenant un point + meilleure lisibilité

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask, send_from_directory
 from werkzeug.middleware.proxy_fix import ProxyFix
+import os
 
 from .api import api
 from . import ics
 from .rss import get_rss
 
 
-app = Flask(__name__, static_folder='../../front/dist/assets')
+front_dir = '../../front/dist/'
+
+
+# Main app
+app = Flask(__name__, static_folder=front_dir + 'assets')
 app.register_blueprint(api, url_prefix='/api')
 
 
@@ -14,23 +19,23 @@ app.register_blueprint(api, url_prefix='/api')
 app.route('/events.ics', methods=['GET'])(ics.get_events)
 app.route('/assos/<id>/events.ics', methods=['GET'])(ics.get_asso_events)
 
+
 # RSS Feed
 app.route('/events.rss')(get_rss)
 
+
 # Front-end
 @app.route('/<path:path>')
+@app.route('/', defaults={'path': ''})
 def get_front(path):
-    if path.split('/')[-1].find('.') == -1:
+    if not os.path.exists(front_dir + path):
         path = 'index.html'
-    return send_from_directory('../../front/dist', path)
-
-@app.route('/')
-def get_index():
-    return send_from_directory('../../front/dist', 'index.html')
+    return send_from_directory(front_dir, path)
 
 @app.route('/displayer')
 def get_displayer_front():
-    return send_from_directory('../../front/dist', 'displayer.html')
+    return send_from_directory(front_dir, 'displayer.html')
+
 
 # Data
 @app.route('/data/<path:path>')
